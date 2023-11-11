761e7096e98eb280bce188ec64af8ac9b7174997
Isaac J Moreira <120274547+IsaacJMoreira@users.noreply.github.com>
2023-11-11 01:37:43 -0300

    EDIT: Gerenciar Eventos TAB. Now the component has editor and deleter.

--- abrace_adm_app/src/components/EventCard/EventCard.tsx
+++ abrace_adm_app/src/components/EventCard/EventCard.tsx
@@ -10,15 +10,24 @@ import {
   Link,
   Box,
   Heading,
+  Separator,
+  Tooltip,
+  TextField,
 } from "@radix-ui/themes";
 import NextImage, { StaticImageData } from "next/image";
 
 import DONATEIcon from "../../public/assets/icons/2904845 1.png";
 
-import { FC, ReactNode, useEffect } from "react";
+import { FC, MouseEventHandler, ReactNode, useEffect, useState } from "react";
 import React from "react";
 
-import { ChevronLeftIcon, HeartFilledIcon } from "@radix-ui/react-icons";
+import {
+  ChevronLeftIcon,
+  DotsHorizontalIcon,
+  HeartFilledIcon,
+  Pencil1Icon,
+  TrashIcon,
+} from "@radix-ui/react-icons";
 
 interface IMGProps {
   URL?: string;
@@ -38,32 +47,241 @@ interface MyProps {
   URL: StaticImageData;
   ALT: string;
   eventID?: string;
-}
-
-interface DisclaimerProps {
-  eventID?: string;
+  deleterFunction?: MouseEventHandler<HTMLButtonElement>;
 }
 
 const EventCard: FC<MyProps> = (props) => {
+  const [eventData, setEventData] = useState({} as MyProps);
+  const [updatedData, setUpdatedData] = useState({} as MyProps);
+
+  const firstLoadDataMOCK = {
+    name: props.name,
+    date: props.time,
+    time: props.time,
+    duration: props.duration,
+    location: props.location,
+    description: props.description,
+    imgURL: props.imgURL,
+    linkToLocation: props.linkToLocation,
+    URL: props.URL,
+    ALT: props.ALT,
+    eventID: props.eventID,
+  };
+
+  useEffect(() => {
+    setEventData(firstLoadDataMOCK);
+    setUpdatedData(firstLoadDataMOCK);
+  }, []);
+
   return (
-    <Card variant="surface">
+    <Card variant="classic">
       <Flex direction="row" justify="between" gap="2">
-        <Inset clip="border-box" side="left" pb="0">
-          <NextImage className="w-[200px]" src={props.URL} alt={props.ALT} />
+        <Inset clip="padding-box" side="left">
+          <NextImage
+            className="object-cover h-40 w-auto"
+            src={eventData.URL}
+            alt={eventData.ALT}
+          />
         </Inset>
-        <Box>
-          <Flex direction="column" justify="start" style={{width: 200}}>
-            <Heading size="4" color="amber">
-              {props.name}
-            </Heading>
-          </Flex>
-        </Box>
-        <Box>
-          <Flex direction="column" justify="between" gap={"1"} align="center">
-            <Button>X</Button>
-            <Button>!</Button>
-          </Flex>
-        </Box>
+        <Flex direction="row" justify="between" className="w-full">
+          <Box>
+            <Flex direction="column" justify="start">
+              <Heading color="amber">{eventData.name}</Heading>
+              <Separator orientation="horizontal" size="4" />
+              <Text as="label" size="1" weight="bold" color="amber">
+                Data:{" "}
+                <Text as="span" size="3" weight="light" color="gray">
+                  {eventData.date}
+                </Text>
+              </Text>
+              <Text as="label" size="1" weight="bold" color="amber">
+                Horário:{" "}
+                <Text as="span" size="3" weight="light" color="gray">
+                  {eventData.time}
+                </Text>
+              </Text>
+              <Text as="label" size="1" weight="bold" color="amber">
+                Duração:{" "}
+                <Text as="span" size="3" weight="light" color="gray">
+                  {eventData.duration}
+                </Text>
+              </Text>
+              <Text as="label" size="1" weight="bold" color="amber">
+                Local:{" "}
+                <Text as="span" size="3" weight="light" color="gray">
+                  {eventData.location}
+                </Text>
+              </Text>
+            </Flex>
+          </Box>
+          <Box>
+            <Flex direction="column" justify="center" gap="1" align="center" className="h-full">
+              <Dialog.Root>
+                <Dialog.Trigger>
+                  <Button color="tomato" variant="surface">
+                    <Tooltip content="Deletar">
+                      <TrashIcon />
+                    </Tooltip>
+                  </Button>
+                </Dialog.Trigger>
+                <Dialog.Content className="p-5" style={{ maxWidth: 300 }}>
+                  <Dialog.Title>Deletar Evento?</Dialog.Title>
+                  <Separator orientation="horizontal" size="4" />
+                  <br />
+                  <Flex
+                    direction="row"
+                    justify="between"
+                    gap="5"
+                    align="center"
+                  >
+                    <Dialog.Close>
+                      <Button
+                        color="tomato"
+                        variant="surface"
+                        onClick={props.deleterFunction}
+                      >
+                        DELETAR
+                      </Button>
+                    </Dialog.Close>
+                    <Dialog.Close>
+                      <Button color="amber" variant="surface">
+                        VOLTAR
+                      </Button>
+                    </Dialog.Close>
+                  </Flex>
+                </Dialog.Content>
+              </Dialog.Root>
+              <Dialog.Root>
+                <Dialog.Trigger>
+                  <Button color="amber" variant="surface">
+                    <Tooltip content="Editar">
+                      <Pencil1Icon />
+                    </Tooltip>
+                  </Button>
+                </Dialog.Trigger>
+                <Dialog.Content>
+                  <Dialog.Title>Editar Evento</Dialog.Title>
+                  <Separator orientation="horizontal" size="4" />
+                  <br />
+                  <Flex direction="column" justify="between" gap="1">
+                    <TextField.Root>
+                      <TextField.Slot>Nome:</TextField.Slot>
+                      <TextField.Input
+                        value={updatedData.name}
+                        onChange={(event) =>
+                          setUpdatedData({
+                            ...updatedData,
+                            name: event.target.value,
+                          })
+                        }
+                      />
+                    </TextField.Root>
+                    <TextField.Root>
+                      <TextField.Slot>Data:</TextField.Slot>
+                      <TextField.Input
+                        value={updatedData.date}
+                        onChange={(event) =>
+                          setUpdatedData({
+                            ...updatedData,
+                            date: event.target.value,
+                          })
+                        }
+                      />
+                    </TextField.Root>
+                    <TextField.Root>
+                      <TextField.Slot>Horário:</TextField.Slot>
+                      <TextField.Input
+                        value={updatedData.time}
+                        onChange={(event) =>
+                          setUpdatedData({
+                            ...updatedData,
+                            date: event.target.value,
+                          })
+                        }
+                      />
+                    </TextField.Root>
+                    <TextField.Root>
+                      <TextField.Slot>Duração:</TextField.Slot>
+                      <TextField.Input
+                        value={updatedData.duration}
+                        onChange={(event) =>
+                          setUpdatedData({
+                            ...updatedData,
+                            duration: event.target.value,
+                          })
+                        }
+                      />
+                    </TextField.Root>
+                    <TextField.Root>
+                      <TextField.Slot>Local:</TextField.Slot>
+                      <TextField.Input
+                        value={updatedData.location}
+                        onChange={(event) =>
+                          setUpdatedData({
+                            ...updatedData,
+                            location: event.target.value,
+                          })
+                        }
+                      />
+                    </TextField.Root>
+                    <TextField.Root>
+                      <TextField.Slot>Link do Google Maps:</TextField.Slot>
+                      <TextField.Input
+                        value={updatedData.linkToLocation}
+                        onChange={(event) =>
+                          setUpdatedData({
+                            ...updatedData,
+                            linkToLocation: event.target.value,
+                          })
+                        }
+                      />
+                    </TextField.Root>
+                    <TextField.Root>
+                      <TextField.Slot>upload de imágem:</TextField.Slot>
+                      <TextField.Input value="MÉTODO PARA UPLOAD DE IMAGEM VERCEL BLOB" />
+                    </TextField.Root>
+                    <TextField.Root>
+                      <TextField.Slot>Descrição da imágem:</TextField.Slot>
+                      <TextField.Input
+                        value={updatedData.ALT}
+                        onChange={(event) =>
+                          setUpdatedData({
+                            ...updatedData,
+                            ALT: event.target.value,
+                          })
+                        }
+                      />
+                    </TextField.Root>
+                  </Flex>
+                  <br />
+                  <Separator orientation="horizontal" size="4" />
+                  <br />
+                  <Flex
+                    direction="row"
+                    justify="between"
+                    gap="5"
+                    align="center"
+                  >
+                    <Dialog.Close>
+                      <Button
+                        color="lime"
+                        variant="surface"
+                        onClick={() => setEventData(updatedData)}
+                      >
+                        ATUALIZAR
+                      </Button>
+                    </Dialog.Close>
+                    <Dialog.Close>
+                      <Button color="amber" variant="surface">
+                        VOLTAR
+                      </Button>
+                    </Dialog.Close>
+                  </Flex>
+                </Dialog.Content>
+              </Dialog.Root>
+            </Flex>
+          </Box>
+        </Flex>
       </Flex>
     </Card>
   );
--- abrace_adm_app/src/components/EventsCard/EventsCard.tsx
+++ abrace_adm_app/src/components/EventsCard/EventsCard.tsx
@@ -1,23 +1,64 @@
-import EventCard from '@/components/EventCard/EventCard'
-import { Card } from '@radix-ui/themes'
-import React from 'react'
-import EXAMPLEimg from '../../public/images/Volunteer.jpg'
+import EventCard from "@/components/EventCard/EventCard";
+import { Card, Flex, Heading, Separator } from "@radix-ui/themes";
+import React from "react";
+import EXAMPLEimg from "../../public/images/Volunteer.jpg";
+
+let events = [
+  {
+    name: "Evento de Adoção 1",
+    URL: EXAMPLEimg,
+    ALT: "ALT DA IMAGEM",
+    date: "29/01/1993",
+    time: "23:30",
+    duration: "3h",
+    location: "Praça das Flores",
+    description:
+      "EVENTO FODA EVENTO FODAEVENTO FODAEVENTO FODAEVENTO FODAEVENTO FODAEVENTO FODAEVENTO FODAEVENTO FODAEVENTO FODAEVENTO FODAEVENTO FODAEVENTO FODAEVENTO FODA",
+    linkToLocation: "www.link.com.br",
+  },
+  {
+    name: "Evento de Adoção 2",
+    URL: EXAMPLEimg,
+    ALT: "ALT DA IMAGEM",
+    date: "29/02/1993",
+    time: "00:30",
+    duration: "5h",
+    location: "Praça Luiza Távora",
+    description:
+      "EVENTO FODA EVENTO FODAEVENTO FODAEVENTO FODAEVENTO FODAEVENTO FODAEVENTO FODAEVENTO FODAEVENTO FODAEVENTO FODAEVENTO FODAEVENTO FODAEVENTO FODAEVENTO FODA",
+    linkToLocation: "www.link.com.br",
+  },
+];
 
 const EventsCard = () => {
   return (
-    <Card variant="ghost" className="bg-gradient-to-br from-[#FFF9C4] to-[#F7EB7Af0]  p-3" style={{ width: 600 }}>
-        <EventCard
-        name='nome'
-        URL={EXAMPLEimg}
-        ALT='ALT DA IMAGEM'
-        date="date"
-        time="hora"
-        duration='3h'
-        location='localização'
-        description='evento foda'
-        linkToLocation='www.link.com.br'/>
-    </Card>
-  )
-}
+    <Card variant="surface">
+      <Flex direction="row" justify="between">
+        <Heading color="amber">Eventos Cadastrados</Heading>
+      </Flex>
 
-export default EventsCard
+      <Separator orientation="horizontal" size="4" />
+      <br />
+      <Flex direction="column" gap="1">
+        {events.map((event) => {
+          return (
+            <EventCard
+              name={event.name}
+              URL={event.URL}
+              ALT={event.ALT}
+              date={event.date}
+              time={event.time}
+              duration={event.duration}
+              location={event.location}
+              description={event.description}
+              linkToLocation={event.linkToLocation}
+              deleterFunction={()=>alert("Ainda Não posso deletar")}
+            />
+          );
+        })}
+      </Flex>
+    </Card>
+  );
+};
+
+export default EventsCard;
