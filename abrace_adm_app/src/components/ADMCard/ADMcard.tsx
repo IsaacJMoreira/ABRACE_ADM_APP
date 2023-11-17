100afd012aaa8fedb80f155cf2f238a2953e0bd1
Isaac J Moreira <120274547+IsaacJMoreira@users.noreply.github.com>
2023-11-17 02:16:09 -0300

    EDIT: the only variant alowed to button in with AlertDialog is surface. Now its confirmed that Radix UI has some kind of fault

--- abrace_adm_app/src/components/ADMCard/ADMcard.tsx
+++ abrace_adm_app/src/components/ADMCard/ADMcard.tsx
@@ -1,4 +1,15 @@
-import { AlertDialog, Box, Button, Card, Dialog, Flex, Separator, Strong, Text } from "@radix-ui/themes";
+import {
+  AlertDialog,
+  Box,
+  Button,
+  Card,
+  Dialog,
+  DialogTitle,
+  Flex,
+  Separator,
+  Strong,
+  Text,
+} from "@radix-ui/themes";
 import React, { FC } from "react";
 import NextImage, { StaticImageData } from "next/image";
 import ADM1 from "../../public/icons/ADMlevel1.png";
@@ -38,7 +49,9 @@ const ADMcard: FC<ADMCardProps> = (props) => {
           </Flex>
         </Box>
         <Box className="min-w-[150px]">
-          <Text>{props.name}</Text>
+          <Text size="4" color="amber" weight="bold">
+            {props.name}
+          </Text>
         </Box>
         <Box className="min-w-[50px]">
           <Flex direction="row" gap="1" justify="between" align="center">
@@ -46,33 +59,74 @@ const ADMcard: FC<ADMCardProps> = (props) => {
             <Flex direction="column" justify="between" gap="1">
               {/* //Habilitar e desabilitar botões dependendo da credencial */}
 
-              <Button disabled={false} size='1' color='amber'>PROMOVER</Button>
-              <Button disabled={false} size="1" color="tomato">
-                EXCLUIR
-              </Button>
-              <AlertDialog.Root >
-                <AlertDialog.Trigger >
-                    <Button>triger</Button>
+              <AlertDialog.Root>
+                <AlertDialog.Trigger disabled={false}>
+                  <Button variant="surface" size="1" color="amber">
+                    {props.credential === "ADM1" ? "PROMOVER" : "REBAIXAR"}
+                  </Button>
                 </AlertDialog.Trigger>
-                <AlertDialog.Content size='1'>
-                    <AlertDialog.Title size='3'>
-                        Tem certeza que deseja excluir {props.name}?
-                    </AlertDialog.Title>
-                    <AlertDialog.Description size='1'>
-                        Esta ação não poderá ser desfeita. Depois de excluir <Strong>{props.name}</Strong>, a conta não poderar ser recuperada.
-                    </AlertDialog.Description>
-                    <br />
-                    <Separator  orientation="horizontal" size='4'/>
-                    <br />
-                    <Flex direction='row' justify='between' align='center'>
-                        <AlertDialog.Action>
-                            <Button color="tomato">EXCLUIR</Button>
-                        </AlertDialog.Action>
-                        <AlertDialog.Cancel>
-                            <Button color="amber">CANCELAR</Button>
-                        </AlertDialog.Cancel>
-                    </Flex>
+                <AlertDialog.Content size="1">
+                  <AlertDialog.Title size="3">
+                    Tem certeza que deseja{" "}
+                    {props.credential === "ADM1" ? "PROMOVER" : "REBAIXAR"}{" "}
+                    {props.name}?
+                  </AlertDialog.Title>
+                  <AlertDialog.Description size="1">
+                    Esta ação modificará o nível de permissões de{" "}
+                    <Strong>{props.name}</Strong> dentro da plataforma.
+                    Administradores de <Strong>Nível 2</Strong> possuem perissão para modificar
+                    todas as informações no APP, inlusive relacionadas à
+                    pagamentos. É de extrema importância que esta credencial só
+                    seja concedida à membros de confiança.
+                  </AlertDialog.Description>
+                  <br />
+                  <Separator orientation="horizontal" size="4" />
+                  <br />
+                  <Flex direction="row" justify="between" align="center">
+                    <AlertDialog.Action>
+                      <Button variant="surface" color="lime">
+                      {props.credential === "ADM1" ? "PROMOVER" : "REBAIXAR"}
+                      </Button>
+                    </AlertDialog.Action>
+                    <AlertDialog.Cancel>
+                      <Button variant="surface" color="amber">
+                        CANCELAR
+                      </Button>
+                    </AlertDialog.Cancel>
+                  </Flex>
+                </AlertDialog.Content>
+              </AlertDialog.Root>
 
+              <AlertDialog.Root>
+                <AlertDialog.Trigger disabled={false}>
+                  <Button variant="surface" size="1" color="tomato">
+                    EXCLUIR
+                  </Button>
+                </AlertDialog.Trigger>
+                <AlertDialog.Content size="1">
+                  <AlertDialog.Title size="3">
+                    Tem certeza que deseja excluir {props.name}?
+                  </AlertDialog.Title>
+                  <AlertDialog.Description size="1">
+                    Esta ação não poderá ser desfeita. Depois de excluir{" "}
+                    <Strong>{props.name}</Strong>, a conta não poderar ser
+                    recuperada.
+                  </AlertDialog.Description>
+                  <br />
+                  <Separator orientation="horizontal" size="4" />
+                  <br />
+                  <Flex direction="row" justify="between" align="center">
+                    <AlertDialog.Action>
+                      <Button variant="surface" color="tomato">
+                        EXCLUIR
+                      </Button>
+                    </AlertDialog.Action>
+                    <AlertDialog.Cancel>
+                      <Button variant="surface" color="amber">
+                        CANCELAR
+                      </Button>
+                    </AlertDialog.Cancel>
+                  </Flex>
                 </AlertDialog.Content>
               </AlertDialog.Root>
             </Flex>
